d14fe84f7894ecefa5999909409fefd866124c13
BehnamEbrahimi <behnam.ebrahimi@gmail.com>
2020-07-16 17:49:05 +1000

    Updated

---
+++ src/events/order-created-event.ts
@@ -0,0 +1,17 @@
+import { Subjects } from "./subjects";
+import { OrderStatus } from "./types/order-status";
+
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
+  data: {
+    // actually status is not needed by other service in this event, we future-proof ourselves by sharing maximum data.
+    id: string;
+    status: OrderStatus;
+    userId: string;
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    };
+  };
+}
