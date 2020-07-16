69f50340fc0a53f3aa430fe3496ddda2a19bc11e
BehnamEbrahimi <behnam.ebrahimi@gmail.com>
2020-07-16 17:50:57 +1000

    Updated

--- src/events/order-cancelled-event.ts
+++ src/events/order-cancelled-event.ts
@@ -3,6 +3,7 @@ import { Subjects } from "./subjects";
 export interface OrderCancelledEvent {
   subject: Subjects.OrderCancelled;
   data: {
+    // bare minimum data that other services need.
     id: string;
     ticket: {
       id: string;
